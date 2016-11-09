36dd41adda7e2eabc4705548b3f6ff5bf034209a
denisoetken <denisoetken@hotmail.de>
2016-11-09 10:53:09 +0100

    ersterbaum

---
+++ src/Baum.java
@@ -0,0 +1,24 @@
+/**
+ * Created by doetken on 09.11.2016.
+ */
+public class Baum {
+
+    private Knoten wurzel;
+
+    public boolean isEmpty() {
+        boolean wert = false;
+
+        return wert;
+    }
+
+    public void add(int pos) {
+
+    }
+
+    public int size() {
+        int groesse = 0;
+
+        return groesse;
+    }
+
+}
---
+++ src/Knoten.java
@@ -0,0 +1,8 @@
+/**
+ * Created by doetken on 09.11.2016.
+ */
+public class Knoten {
+    private int wert;
+    private Knoten rechts;
+    private Knoten links;
+}
